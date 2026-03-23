59cf3457e432a4f219a0c80c5b4326061b81dd4e
franckmonmardy-cell <franckmonmardy@gmail.com>
2026-03-23 21:09:00 +0000

    Update programme.js

--- api/api/programme.js
+++ api/api/programme.js
@@ -14,7 +14,13 @@ module.exports = async (req, res) => {
     res.setHeader('Cache-Control', 's-maxage=120');
     return res.status(200).json(data);
   } catch(e) {
-    return res.status(500).json({ error: e.message });
+    // Fallback: essayer online
+    try {
+      const data2 = await apiCall2(`/rest/client/61/programme/${date}`);
+      return res.status(200).json(data2);
+    } catch(e2) {
+      return res.status(500).json({ error: e2.message });
+    }
   }
 };
 
@@ -23,7 +29,37 @@ function apiCall(path) {
     const req = https.request({
       hostname: 'offline.turfinfo.api.pmu.fr',
       port: 443, path, method: 'GET',
-      headers: { 'User-Agent': 'Mozilla/5.0', 'Accept': 'application/json' }
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10)',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'fr-FR,fr;q=0.9',
+        'Origin': 'https://www.pmu.fr',
+        'Referer': 'https://www.pmu.fr/'
+      }
+    }, (res) => {
+      let data = '';
+      res.on('data', c => data += c);
+      res.on('end', () => {
+        try { resolve(JSON.parse(data)); }
+        catch(e) { reject(new Error('Parse error: '+data.substring(0,200))); }
+      });
+    });
+    req.on('error', reject);
+    req.end();
+  });
+}
+
+function apiCall2(path) {
+  return new Promise((resolve, reject) => {
+    const req = https.request({
+      hostname: 'online.turfinfo.api.pmu.fr',
+      port: 443, path, method: 'GET',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10)',
+        'Accept': 'application/json',
+        'Origin': 'https://www.pmu.fr',
+        'Referer': 'https://www.pmu.fr/'
+      }
     }, (res) => {
       let data = '';
       res.on('data', c => data += c);
